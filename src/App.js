79030be031a0949f729be8811b31f09a4b749c41
lakhwinder555 <lakhwindersinghwins@gmail.com>
2024-09-03 13:39:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to DITS</h1>
         </header>
         <p className="App-intro">
-           <code>src/Sucessful</code> 
+           <code>Sucessful</code> 
         </p>
       </div>
     );
